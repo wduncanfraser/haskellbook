b85a9a0489b7db015306c573c5534b5650458fe4
W. Duncan Fraser <duncan@wduncanfraser.com>
2017-04-13 13:48:48 -0800

    Added exercises and notes from Chapter 2 and 3.

---
+++ 2/211exercises.hs
@@ -0,0 +1,27 @@
+-- 2 + (2 * 3) - 3
+
+-- -- 1
+-- 2 + (2 * 3) -1
+
+-- -- 2
+-- (^) 10 (1 + 1)
+
+-- -- 3
+-- ((2 ^ 2) * (4 ^ 5)) + 1
+
+-- More fun with functions
+-- let z = 7
+-- let y = z + 8
+-- let x = y ^ 2
+-- let waxOn = x * 5
+
+-- let triple x = x * 3
+
+wax0n = x * 5
+  where z = 7
+        y = z + 8
+        x = y ^ 2
+
+triple x = x * 3
+
+wax0ff x = triple x
